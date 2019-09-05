228b30f65bea5bea28edb06c4d920b01f214b343
Daniel Dihardja <daniel@dihardja.de>
2019-09-05 19:19:17 +0200

    only upload after 60 sec...

--- save-image.js
+++ save-image.js
@@ -4,7 +4,13 @@
 
 import {config} from "./config";
 
+let lastUpload = 0;
+
 export const saveImage = (canvas) => {
+  if(Date.now() - lastUpload < 60 * 1000) {
+    return;
+  }
+  lastUpload = Date.now();
   canvas.toBlob(blob => {
     const formData = new FormData();
     formData.append('sd-image', blob, 'sdimage.png');
